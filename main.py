a4390f73822829828775fb701df7705c37881ba9
hermangavrylov <gavrylovherman@gmail.com>
2025-04-18 10:07:29 +0200

    Create Instances

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv")
+
 class Hotel:
+
+    def __init__(self, id):
+        pass
+
+    def available(self):
+        pass
 
     def view(self):
         pass
@@ -8,5 +18,22 @@ class Hotel:
 
 
 class Reservation:
+
+    def __init(self, customer_name, hotel_object):
+        pass
+
     def generare(self):
         pass
+
+
+print(df)
+id = input("Enter the id of the hotel: ")
+hotel = Hotel(id)
+
+if hotel.available:
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation = Reservation(name, hotel)
+    print(reservation.generate())
+else:
+    print("Hotel is not free.")
